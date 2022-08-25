dfe4825ed0a173332c38aea767e5e51faf1dd044
MicahD18 <micah.daise@nwktc.edu>
2022-08-25 17:03:10 -0600

    added toggleDone() method

--- todo-app/src/app/components/todo/todo.component.ts
+++ todo-app/src/app/components/todo/todo.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
+  
+  todos: Todo[] | undefined;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.todos = [
+      { content: 'Code', completed: true },
+      { content: 'Learn Angular', completed: false },
+      { content: "Learn Rubik's Cube", completed: false },
+    ];
+  }
+
+  toggleDone(id: number) {
+    this.todos?.map((value, index) => {
+      if (index === id) {
+        // sets completed to true
+        value.completed = !value.completed;
+      }
+      return value;
+    })
   }
 
 }
